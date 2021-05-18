fe693a59bab15c07328ce9df2e17d0551d20cc1e
Yuriy Dvorzhetskiy <dvorzhetskij@mail.ru>
2021-05-18 21:26:11 +0600

    Hello world

---
+++ src/main/java/ru/otus/example/OtusSpringDockerApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtusSpringDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtusSpringDockerApplication.class, args);
+	}
+
+}
