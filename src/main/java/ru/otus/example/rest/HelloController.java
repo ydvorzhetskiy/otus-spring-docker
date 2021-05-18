62ab170a3ee80895e88766fabda53c92c100fd78
Yuriy Dvorzhetskiy <dvorzhetskij@mail.ru>
2021-05-18 22:45:57 +0600

    Docker file

---
+++ src/main/java/ru/otus/example/rest/HelloController.java
@@ -0,0 +1,23 @@
+package ru.otus.example.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+
+    private final String hello;
+
+    public HelloController(@Value("${application.hello-string") String hello) {
+        this.hello = hello;
+    }
+
+    @GetMapping("/hello")
+    public Map<String, String> hello() {
+        return Collections.singletonMap("hello", hello);
+    }
+}
